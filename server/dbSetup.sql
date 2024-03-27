6683da17f5e44212f1662cf1f1ee66d26d3a276a
Seth Kunzler <sethkunzler@gmail.com>
2024-03-27 14:34:32 -0600

    Recipe Table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipe {
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title VARCHAR(50) NOT NULL COMMENT 'Recipe Title',
+  instructions VARCHAR(1000) NOT NULL,
+  img VARCHAR(500) NOT NULL,
+  category SET("soups", "pasta", "fish", "salads", "beef", "burger", "pasta", "sandwich", "breakfast", "lunch", "dinner", "mexican", "italian", "chinese", "entrees", "snack", "dessert", "appetizer", "meal") NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+}
+
+SELECT * FROM accounts
