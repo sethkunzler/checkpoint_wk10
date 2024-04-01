dbc6fd11e6db7b482412d0002afa0138cc1f2a61
Seth Kunzler <sethkunzler@gmail.com>
2024-04-01 15:53:45 -0600

    Recipe Basic Template Complete

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Week 10 Checkpoint
-      </h1>
+    <div class="container">
+      <section class="row">
+        <div class="col-md-12">
+          <h1>Browse Recipes</h1>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <h2>Tacos</h2>
+            <img class="recipe-card-img" src="https://images.unsplash.com/photo-1624300629298-e9de39c13be5?q=80&w=2500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="mexican tacos">
+            <div class="pt-2">
+              <span class="">Mexican</span>
+            </div>
+            <div class="d-flex justify-content-end mb-1">
+              <span v-if="isLiked" class="mdi mdi-heart text-danger selectable rounded fs-4 px-2 mx-2"></span>
+              <span class="mdi mdi-heart-outline selectable rounded fs-4 px-2 mx-2"></span>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -13,8 +26,9 @@
 <script>
 export default {
   setup() {
+    const isLiked = false
     return {
-      
+      isLiked
     }
   }
 }
@@ -38,6 +52,15 @@ export default {
       object-fit: contain;
       object-position: center;
     }
+  }
+  .mdi-heart-outline:hover {
+    color: red;
+  }
+  .recipe-card-img {
+    width: 100%;
+    height: 40dvh;
+    object-fit: cover;
+    object-position: center;
   }
 }
 </style>
