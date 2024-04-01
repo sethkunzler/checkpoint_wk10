4eea1c5669c6102a1148feee33c522d8072bd2f1
Seth Kunzler <sethkunzler@gmail.com>
2024-04-01 17:17:46 -0600

    some styling

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,27 +1,22 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link>
+  <nav class="navbar navbar-expand-sm px-3">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
-        <li>
+        <li class="bg-dark rounded-top mx-2">
           <router-link :to="{ name: 'Home' }" class="btn text-success lighten-30 selectable text-uppercase">
             Home
           </router-link>
         </li>
-        <li>
+        <li class="bg-dark rounded-top mx-2">
           <router-link :to="{ name: 'My Recipes' }" class="btn text-success lighten-30 selectable text-uppercase">
             My Recipes
           </router-link>
         </li>
-        <li>
+        <li class="bg-dark rounded-top mx-2">
           <router-link :to="{ name: 'Favorites' }" class="btn text-success lighten-30 selectable text-uppercase">
             Favorites
           </router-link>
@@ -65,6 +60,16 @@ export default {
 </script>
 
 <style scoped>
+.navbar {
+  background-image: url("https://images.unsplash.com/photo-1496701213998-7fd1bc5528b5?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-color: rgba(0, 0, 0, 0.40);
+  background-blend-mode: color-burn;
+  padding: 16px;
+}
+
 a:hover {
   text-decoration: none;
 }
